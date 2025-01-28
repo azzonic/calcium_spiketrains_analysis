399c58df534841d45809c253f86873456b92351b
azzonic <caterina.azzoni@mdc-berlin.de>
2025-01-28 15:22:08 +0100

    Update datetime_setup.py

--- datetime_setup.py
+++ datetime_setup.py
@@ -2,15 +2,17 @@ import datetime  # Importing datetime for working with dates and time
 
 # Set up date and experiment parameters
 date = datetime.date(2024, 8, 9)
-experiment = "E2_3"
+experiment = "E2"
 
 # Format the date components
 year = str(date.year)
 month = f"{date.month:02d}"
 day = f"{date.day:02d}"
 
+#change your path in the following lines: 
+
 folder_name = rf"{year}{month}{day}"
-analysed_data = rf"C:\Users\azzonic\Desktop\PhD_Falcke\experiments\Ca2imag_CalB_CCh_HEK\ISIscriptanalysed_data/CChCB_{year}_{month}_{day}_{experiment}_analysis.csv"
+analysed_data = rf"yourpath/CChCB_{year}_{month}_{day}_{experiment}_analysis.csv"
 file_name = f"{year}{month}{day}{experiment}"
-pycasig_df = rf"C:\Users\azzonic\Desktop\PhD_Falcke\experiments\Ca2imag_CalB_CCh_HEK\ISI\PyCaSig_analysis/{folder_name}/{file_name}.csv"
-save_path = rf"C:\Users\azzonic\Desktop\PhD_Falcke\experiments\Ca2imag_CalB_CCh_HEK\ISI\PyCaSig_analysis/{folder_name}/{file_name}"
+pycasig_df = rf"yourpath/{folder_name}/{file_name}.csv"
+save_path = rf"yourpath/{folder_name}/{file_name}"
